9c17dd107a3175486d456d40b18054f96628f3bb
whishiwassmartenoughtocodevoidermc <rmrenfbg@gmail.com>
2024-07-05 08:03:31 -0700

    time to add recipes

--- src/main/java/net/robbie/rubysteelmod/item/moditem.java
+++ src/main/java/net/robbie/rubysteelmod/item/moditem.java
@@ -3,6 +3,8 @@ package net.robbie.rubysteelmod.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
@@ -11,6 +13,9 @@ import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
@@ -20,6 +25,8 @@ import net.minecraft.entity.LivingEntity;
 import  net.minecraft.entity.projectile.PersistentProjectileEntity;
 
 import java.util.function.Predicate;
+
+import static net.minecraft.command.EntitySelectorReader.RANDOM;
 
 public class moditem {
     public static final Item REDSTEEL = registeritem("redsteel",new Item(new FabricItemSettings()));
@@ -107,35 +114,65 @@ public class moditem {
         }
     }
 
-    public static class RubyBowItem extends BowItem {
-        public RubyBowItem(Settings settings) {
-            super(settings);
-        }
+public static class RubyBowItem extends BowItem {
+    public RubyBowItem(Settings settings) {
+        super(settings);
+    }
 
-        @Override
-        public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
-            if (user instanceof PlayerEntity) {
-                PlayerEntity playerEntity = (PlayerEntity)user;
-                ItemStack itemStack = getRubyArrow(playerEntity);
+    @Override
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
+        if (user instanceof PlayerEntity) {
+            PlayerEntity playerEntity = (PlayerEntity)user;
+            boolean creativeMode = playerEntity.getAbilities().creativeMode;
+            ItemStack itemStack = playerEntity.getMainHandStack();
 
+            if (!itemStack.isEmpty() || creativeMode) {
                 if (itemStack.isEmpty()) {
-                    return;
+                    itemStack = new ItemStack(moditem.RUBY_ARROW);  // Only use ruby arrows
                 }
 
-                // Your existing code...
-            }
-        }
+                int i = this.getMaxUseTime(stack) - remainingUseTicks;
+                float f = getPullProgress(i);
 
-        private ItemStack getRubyArrow(PlayerEntity playerEntity) {
-            Predicate<ItemStack> predicate = (itemStack) -> itemStack.getItem() == RUBY_ARROW;
-            ItemStack arrowStack = BowItem.getHeldProjectile(playerEntity, predicate);
-            if (arrowStack.isEmpty()) {
-                predicate = (itemStack) -> itemStack.getItem() instanceof ArrowItem;
-                arrowStack = BowItem.getHeldProjectile(playerEntity, predicate);
+                if ((double)f >= 0.1D) {
+                    boolean bl = creativeMode && itemStack.getItem() == moditem.RUBY_ARROW;  // Only use ruby arrows
+
+                    if (!world.isClient) {
+                        ArrowItem arrowItem = (ArrowItem)((ArrowItem)(itemStack.getItem() instanceof ArrowItem ? itemStack.getItem() : moditem.RUBY_ARROW));  // Only use ruby arrows
+                        PersistentProjectileEntity persistentProjectileEntity = arrowItem.createArrow(world, itemStack, playerEntity);
+                        persistentProjectileEntity.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getYaw(), 0.0F, f * 3.0F, 1.0F);
+
+                        if (f == 1.0F) {
+                            persistentProjectileEntity.setCritical(true);
+                        }
+
+                        stack.damage(1, playerEntity, (p) -> {
+                            p.sendToolBreakStatus(playerEntity.getActiveHand());
+                        });
+
+                        if (bl || playerEntity.getAbilities().creativeMode && (itemStack.getItem() == moditem.RUBY_ARROW)) {
+                            persistentProjectileEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
+                        }
+
+                        world.spawnEntity(persistentProjectileEntity);
+                    }
+
+                    world.playSound(null, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.PLAYERS, 1.0F, 1.0F / (world.getRandom().nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+                    if (!bl && !playerEntity.getAbilities().creativeMode) {
+                        itemStack.decrement(1);
+                        if (itemStack.isEmpty()) {
+                            playerEntity.getInventory().removeOne(itemStack);
+                        }
+                    }
+
+                    playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+                }
             }
-            return arrowStack;
         }
     }
+}
+
+
 
     public static class RubyArrowItem extends ArrowItem {
         public RubyArrowItem(Settings settings) {
