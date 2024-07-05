45246e703649c1c2330533eb1773e5d6878eb94c
whishiwassmartenoughtocodevoidermc <rmrenfbg@gmail.com>
2024-07-05 09:24:29 -0700

    time to add recipes

--- src/main/java/net/robbie/rubysteelmod/RubySteelMod.java
+++ src/main/java/net/robbie/rubysteelmod/RubySteelMod.java
@@ -1,7 +1,6 @@
 package net.robbie.rubysteelmod;
-
+import net.minecraft.registry.*;
 import net.fabricmc.api.ModInitializer;
-
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -13,27 +12,24 @@ import net.robbie.rubysteelmod.item.moditemgroups;
 import net.robbie.rubysteelmod.world.gen.ModWorldGeneration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import net.minecraft.item.*;
-import net.minecraft.item.Item;
 
 public class RubySteelMod implements ModInitializer {
     public static final String MOD_ID = "rubysteelmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		moditemgroups.registerItemGroups();
-		moditem.registermoditem();
-		modblock.registerModBlocks();
-		ModWorldGeneration.generateModWorldGen();
-		// Register Ruby Bow Item
+    @Override
+    public void onInitialize() {
+        moditemgroups.registerItemGroups();
+        moditem.registermoditem();
+        modblock.registerModBlocks();
+        ModWorldGeneration.generateModWorldGen();
+
+        // Register Ruby Bow Item
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "ruby_bow"), new RubyBowItem(new FabricItemSettings().fireproof()));
 
         // Register Ruby Arrow Item
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "ruby_arrow"), new RubyArrowItem(new FabricItemSettings()));
 
         // Optionally, perform any additional setup with your items here
-
-
     }
 }
