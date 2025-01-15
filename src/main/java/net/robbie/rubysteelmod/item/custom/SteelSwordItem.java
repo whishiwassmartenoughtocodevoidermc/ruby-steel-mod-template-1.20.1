0423d320e5d1243aba0cebd055e4eadf8e5201bc
whishiwassmartenoughtocodevoidermc <rmrenfbg@gmail.com>
2025-01-15 12:01:58 -0700

    revamp

--- src/main/java/net/robbie/rubysteelmod/item/custom/SteelSwordItem.java
+++ src/main/java/net/robbie/rubysteelmod/item/custom/SteelSwordItem.java
@@ -1,5 +1,6 @@
 package net.robbie.rubysteelmod.item.custom;
 
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
@@ -13,17 +14,19 @@ public class SteelSwordItem extends SwordItem {
     public SteelSwordItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
         super(toolMaterial, attackDamage, attackSpeed, settings);
     }
-
-    public boolean postHit( LivingEntity target, LivingEntity attacker) {
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         // Example: Apply custom effects or behaviors on hit
         if (target.isAlive()){
-            target.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS));
+            target.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS,3,2));
+            target.addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER,3,2));
         }
         if (!target.getWorld().isClient()) {
             // Custom logic, e.g., apply fire damage
+             }
+        stack.damage(1, attacker, (e) -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
 
-        }
-        return super.postHit(target, attacker); // Retain default behavior (e.g., damage durability)
+        return super.postHit(stack,target, attacker); // Retain default behavior (e.g., damage durability)
     }
 
 
